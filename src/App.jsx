a9178d590a7927373be3f80ff2f00554bda4051c
Luca Barneto <luca.barneto.uiux@gmail.com>
2023-11-16 19:35:42 -0300

    agregando primeras rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
+import Error404 from "./pages/Error404";
 function App() {
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting="Hello World!" />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting="Lista" />} />
+          <Route
+            path="/category/:id"
+            element={<ItemListContainer greeting="Lista" />}
+          />
+          <Route path="/items/:id" element={<div>Details</div>} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,9 @@
+const Error404 = () => {
+  return (
+    <>
+      <h1>404 Not Found :/ </h1> <p>No pudimos encontrar la página</p>
+    </>
+  );
+};
+
+export default Error404;
